ab90a74d87c0639171b8fc8fffa16c1c62ad7935
axexlck <axelclk@gmail.com>
2021-06-12 12:56:15 +0200

    Add ulp() call

--- ApfloatCalculusField/src/org/symja/experimental/TestHipparchus.java
+++ ApfloatCalculusField/src/org/symja/experimental/TestHipparchus.java
@@ -1,5 +1,7 @@
 package org.symja.experimental;
 
+import org.apfloat.Apfloat;
+import org.apfloat.ApfloatMath;
 import org.hipparchus.special.elliptic.carlson.CarlsonEllipticIntegral;
 
 import junit.framework.TestCase;
@@ -13,6 +15,8 @@ public class TestHipparchus extends TestCase {
   public void testCarlson() {
 	  ApfloatElement x = ApfloatElement.valueOf("3", 25);
 	  ApfloatElement y = ApfloatElement.valueOf("1.234567890123456789012345", 25);
+	  Apfloat ulp = ApfloatMath.ulp(x.fApfloat);
+	  assertEquals("1e-24", ulp.toString());
       ApfloatElement rC = CarlsonEllipticIntegral.rC(x, y);
       assertEquals("", rC.toString());
   }
