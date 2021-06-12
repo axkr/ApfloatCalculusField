9a4c8e9c944a4823b601473342cdc7a90f886680
axexlck <axelclk@gmail.com>
2021-06-12 13:37:53 +0200

    Delegate to more Apfloat methods

--- ApfloatCalculusField/src/org/symja/experimental/ApfloatElement.java
+++ ApfloatCalculusField/src/org/symja/experimental/ApfloatElement.java
@@ -1,5 +1,7 @@
 package org.symja.experimental;
 
+import java.math.RoundingMode;
+
 import org.apfloat.Apcomplex;
 import org.apfloat.Apfloat;
 import org.apfloat.ApfloatMath;
@@ -8,6 +10,7 @@ import org.hipparchus.Field;
 import org.hipparchus.exception.MathIllegalArgumentException;
 import org.hipparchus.exception.MathRuntimeException;
 import org.hipparchus.exception.NullArgumentException;
+import org.hipparchus.util.FastMath;
 import org.hipparchus.util.FieldSinCos;
 import org.hipparchus.util.FieldSinhCosh;
 
@@ -183,23 +186,23 @@ public class ApfloatElement implements CalculusFieldElement<ApfloatElement> {
   }
 
   @Override
-  public ApfloatElement cbrt() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement cbrt() {
+    return valueOf(ApfloatMath.cbrt(fApfloat));
   }
 
   @Override
-  public ApfloatElement ceil() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement ceil() {
+    return valueOf(ApfloatMath.ceil(fApfloat));
   }
 
   @Override
-  public ApfloatElement copySign(ApfloatElement arg0) { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement copySign(ApfloatElement value) {
+    return valueOf(ApfloatMath.copySign(fApfloat, value.fApfloat));
   }
 
   @Override
-  public ApfloatElement copySign(double arg0) { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement copySign(double d) {
+    return valueOf(ApfloatMath.copySign(fApfloat, new Apfloat(d, fApfloat.precision())));
   }
 
   @Override
@@ -208,13 +211,13 @@ public class ApfloatElement implements CalculusFieldElement<ApfloatElement> {
   }
 
   @Override
-  public ApfloatElement expm1() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement expm1() {
+    return valueOf(ApfloatMath.exp(fApfloat).subtract(Apfloat.ONE));
   }
 
   @Override
-  public ApfloatElement floor() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement floor() {
+    return valueOf(ApfloatMath.floor(fApfloat));
   }
 
   @Override
@@ -223,97 +226,98 @@ public class ApfloatElement implements CalculusFieldElement<ApfloatElement> {
   }
 
   @Override
-  public ApfloatElement hypot(ApfloatElement arg0)
-      throws MathIllegalArgumentException { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement hypot(ApfloatElement y) throws MathIllegalArgumentException {
+    return valueOf(
+        ApfloatMath.sqrt(fApfloat.multiply(fApfloat)).add(y.fApfloat.multiply(y.fApfloat)));
   }
 
   @Override
-  public ApfloatElement linearCombination(ApfloatElement[] arg0, ApfloatElement[] arg1)
-      throws MathIllegalArgumentException { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement linearCombination(ApfloatElement[] a, ApfloatElement[] b)
+      throws MathIllegalArgumentException {
+    Apfloat result = Apfloat.ZERO;
+    for (int i = 0; i < a.length; i++) {
+      result = result.add(a[i].fApfloat.multiply(b[i].fApfloat));
+    }
+    return valueOf(result);
   }
 
   @Override
-  public ApfloatElement linearCombination(double[] arg0, ApfloatElement[] arg1)
-      throws MathIllegalArgumentException { // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public ApfloatElement linearCombination(
-      ApfloatElement arg0,
-      ApfloatElement arg1,
-      ApfloatElement arg2,
-      ApfloatElement arg3) { // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public ApfloatElement linearCombination(
-      double arg0,
-      ApfloatElement arg1,
-      double arg2,
-      ApfloatElement arg3) { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement linearCombination(double[] a, ApfloatElement[] b)
+      throws MathIllegalArgumentException {
+    Apfloat result = Apfloat.ZERO;
+    if (b.length > 0) {
+      long precision = b[0].fApfloat.precision();
+      for (int i = 0; i < a.length; i++) {
+        result = result.add(new Apfloat(a[i], precision).multiply(b[i].fApfloat));
+      }
+    }
+    return valueOf(result);
   }
 
   @Override
   public ApfloatElement linearCombination(
-      ApfloatElement arg0,
-      ApfloatElement arg1,
-      ApfloatElement arg2,
-      ApfloatElement arg3,
-      ApfloatElement arg4,
-      ApfloatElement arg5) { // TODO Auto-generated method stub
-    return null;
+      ApfloatElement a1, ApfloatElement b1, ApfloatElement a2, ApfloatElement b2) {
+    return linearCombination(new ApfloatElement[] {a1, a2}, new ApfloatElement[] {b1, b2});
   }
 
   @Override
   public ApfloatElement linearCombination(
-      double arg0,
-      ApfloatElement arg1,
-      double arg2,
-      ApfloatElement arg3,
-      double arg4,
-      ApfloatElement arg5) { // TODO Auto-generated method stub
-    return null;
+      double a1, ApfloatElement b1, double a2, ApfloatElement b2) {
+    return linearCombination(new double[] {a1, a2}, new ApfloatElement[] {b1, b2});
   }
 
   @Override
   public ApfloatElement linearCombination(
-      ApfloatElement arg0,
-      ApfloatElement arg1,
-      ApfloatElement arg2,
-      ApfloatElement arg3,
-      ApfloatElement arg4,
-      ApfloatElement arg5,
-      ApfloatElement arg6,
-      ApfloatElement arg7) { // TODO Auto-generated method stub
-    return null;
+      ApfloatElement a1,
+      ApfloatElement b1,
+      ApfloatElement a2,
+      ApfloatElement b2,
+      ApfloatElement a3,
+      ApfloatElement b3) {
+    return linearCombination(new ApfloatElement[] {a1, a2, a3}, new ApfloatElement[] {b1, b2, b3});
   }
 
   @Override
   public ApfloatElement linearCombination(
-      double arg0,
-      ApfloatElement arg1,
-      double arg2,
-      ApfloatElement arg3,
-      double arg4,
-      ApfloatElement arg5,
-      double arg6,
-      ApfloatElement arg7) { // TODO Auto-generated method stub
-    return null;
+      double a1, ApfloatElement b1, double a2, ApfloatElement b2, double a3, ApfloatElement b3) {
+    return linearCombination(new double[] {a1, a2, a3}, new ApfloatElement[] {b1, b2, b3});
   }
 
   @Override
-  public ApfloatElement log10() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement linearCombination(
+      ApfloatElement a1,
+      ApfloatElement b1,
+      ApfloatElement a2,
+      ApfloatElement b2,
+      ApfloatElement a3,
+      ApfloatElement b3,
+      ApfloatElement a4,
+      ApfloatElement b4) {
+    return linearCombination(
+        new ApfloatElement[] {a1, a2, a3, a4}, new ApfloatElement[] {b1, b2, b3, b4});
   }
 
   @Override
-  public ApfloatElement log1p() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement linearCombination(
+      double a1,
+      ApfloatElement b1,
+      double a2,
+      ApfloatElement b2,
+      double a3,
+      ApfloatElement b3,
+      double a4,
+      ApfloatElement b4) {
+    return linearCombination(new double[] {a1, a2, a3, a4}, new ApfloatElement[] {b1, b2, b3, b4});
+  }
+
+  @Override
+  public ApfloatElement log10() {
+    return valueOf(ApfloatMath.log(fApfloat, new Apfloat(10, fApfloat.precision())));
+  }
+
+  @Override
+  public ApfloatElement log1p() {
+    return valueOf(ApfloatMath.log(fApfloat.add(Apfloat.ONE)));
   }
 
   @Override
@@ -332,8 +336,8 @@ public class ApfloatElement implements CalculusFieldElement<ApfloatElement> {
   }
 
   @Override
-  public ApfloatElement reciprocal() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement reciprocal() {
+    return valueOf(ApfloatMath.inverseRoot(fApfloat, 1));
   }
 
   @Override
@@ -347,13 +351,13 @@ public class ApfloatElement implements CalculusFieldElement<ApfloatElement> {
   }
 
   @Override
-  public ApfloatElement rint() { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement rint() {
+    return valueOf(ApfloatMath.round(fApfloat, fApfloat.precision(), RoundingMode.HALF_EVEN));
   }
 
   @Override
-  public ApfloatElement scalb(int arg0) { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement scalb(int n) {
+    return valueOf(fApfloat.multiply(ApfloatMath.pow(new Apfloat(2), n)));
   }
 
   @Override
@@ -367,13 +371,13 @@ public class ApfloatElement implements CalculusFieldElement<ApfloatElement> {
   }
 
   @Override
-  public ApfloatElement abs() { // TODO Auto-generated method stub
+  public ApfloatElement abs() {
     return valueOf(ApfloatMath.abs(fApfloat));
   }
 
   @Override
-  public ApfloatElement newInstance(double arg0) { // TODO Auto-generated method stub
-    return null;
+  public ApfloatElement newInstance(double d) {
+    return valueOf(d, fApfloat.precision());
   }
 
   @Override
